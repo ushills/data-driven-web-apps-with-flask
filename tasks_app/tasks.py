c226da57ac4cee63104b43b386987610d57e3692
ushills <web@ushills.co.uk>
2019-07-17 11:49:25 +0100

    changed to tasks app

---
+++ tasks_app/tasks.py
@@ -0,0 +1,42 @@
+import flask
+
+app = flask.Flask(__name__)
+
+
+def get_task_data():
+    return [
+        {
+            "action": "next",
+            "title": "Update tasks list",
+            "description": "review list",
+            "waiting": "Tom",
+            "tag": "work",
+        },
+        {
+            "action": "waiting",
+            "title": "Waiting for someone",
+            "description": "who am I waiting for",
+            "waiting": "Mark",
+        },
+        {
+            "action": "next",
+            "title": "Review proposals",
+            "description": "Tom has list on desk",
+        },
+    ]
+
+
+@app.route("/")
+def index():
+    return "Hello World!"
+
+
+@app.route("/tasks")
+def tasks():
+    tasks = get_task_data()
+    # return {"tasks": tasks}
+    return flask.render_template("tasks.html", tasks=tasks)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
